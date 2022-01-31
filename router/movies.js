b01db002d6e87a384ee8fe2683e94c55c460fab9
усачёв артём <bbbiii.444@yandex.ru>
2022-01-31 19:47:58 +0500

    fixed a typo in the router (trailerLink)

--- router/movies.js
+++ router/movies.js
@@ -1,32 +1,38 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const {
-  getMovies, createMovie, deleteMovie,
-} = require('../controllers/movies');
+const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
 const urlValidator = require('../utils/urlValidator');
 
 router.get('/movies', getMovies);
 
-router.post('/movies', celebrate({
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().custom(urlValidator),
-    trailer: Joi.string().required().custom(urlValidator),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
-    thumbnail: Joi.string().required().custom(urlValidator),
-    movieId: Joi.number().required(),
+router.post(
+  '/movies',
+  celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required().custom(urlValidator),
+      trailerLink: Joi.string().required().custom(urlValidator),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+      thumbnail: Joi.string().required().custom(urlValidator),
+      movieId: Joi.number().required(),
+    }),
   }),
-}), createMovie);
+  createMovie,
+);
 
-router.delete('/movies/:movieId', celebrate({
-  params: Joi.object().keys({
-    movieId: Joi.string().length(24).hex(),
+router.delete(
+  '/movies/:movieId',
+  celebrate({
+    params: Joi.object().keys({
+      movieId: Joi.string().length(24).hex(),
+    }),
   }),
-}), deleteMovie);
+  deleteMovie,
+);
 
 module.exports = router;
